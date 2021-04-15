840c6095462836e5a1195eebae4103148927475a
MDaffaY <78152398+MDaffaY@users.noreply.github.com>
2021-04-15 08:55:56 +0700

    Add files via upload

---
+++ PBO/PBO Siswa.py
@@ -0,0 +1,24 @@
+class Siswa:
+	'''Dasar kelas untuk semua karyawan'''
+	jumlah_siswa = 0
+	
+	def __init__(self, nama, NIS):
+		self.nama = nama
+		self.NIS = NIS
+		Siswa.jumlah_siswa += 1
+		
+	def tampilkan_jumlah(self):
+		print("Total siswa:", Siswa.jumlah_siswa)
+		
+	def tampilkan_profil(self):
+		print("Nama :", self.nama)
+		print("NIS :", self.NIS)
+		
+# Membuat objek pertama dari kelas Karyawan
+siswa1 = Siswa("Nuril Karyamin", 16243)
+# Membuat objek kedua dari kelas Karyawan
+siswa2 = Siswa("Fauzan", 16244)
+
+siswa1.tampilkan_profil()
+siswa2.tampilkan_profil()
+print("Total siswa :", Siswa.jumlah_siswa)
